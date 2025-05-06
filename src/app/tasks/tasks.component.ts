46df9f177b9ecaf09d04fe14f7d7616273ee2a45
RaamTechLead-DSAI <braamprekash@gmail.com>
2025-05-06 15:10:56 +0100

    Enhanced to display user specific tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -9,6 +9,33 @@ import { TaskComponent } from "./task/task.component";
   imports: [TaskComponent]
 })
 export class TasksComponent {
+  @Input({ required:true }) userId!:string;
   @Input({ required:true }) name!:string;
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Sales Target',
+      summary: 'Sell 7 cars by end of each month',
+      dueDate: '2025-12-31'
+    },
+    {
+      id: 't2',
+      userId: 'u2',
+      title: 'Service Target',
+      summary: 'Service of each car must be completed within 2 days',
+      dueDate: '2025-12-31'
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Lead Target',
+      summary: 'Identify 30 potential client each month',
+      dueDate: '2025-12-31'
+    },
+  ]
 
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
 }
